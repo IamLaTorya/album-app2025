dbf0de2472748b285b9312e30accd1649dc3783e
IamLaTorya <LaTorya.Sadler@yahoo.com>
2025-11-06 12:21:41 -0600

    added findAlbumInfo method

--- routes/api/albumRoutes.js
+++ routes/api/albumRoutes.js
@@ -7,7 +7,8 @@ const { albumDao: dao } = require('../../daos/dao')
 //http://localhost:3000/api/album
 router.get('/', (req, res)=>
 {
-    dao.findAll(req, res, dao.table)//callback function
+    //dao.findAll(req, res, dao.table)//callback function
+    dao.findAlbumInfo(res, dao.table)
 })
 
 router.get('/sort/:sorter', (req, res)=>
